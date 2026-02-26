010facff9428a35988ac9d631033c13d2fb6a980
draculagorst-rgb <draculagorst@gmail.com>
2026-02-25 22:37:33 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  BOT_NAME: 'Formation',
+  OWNER_NAME: 'Dracula Tech',
+  OWNER_NUMBER: 'Put your number',
+  SESSION_DIR: './baileys',
+  NO_PREFIX: true,
+  STATUS_VIEW: true
+};
+
+
+
+
+
